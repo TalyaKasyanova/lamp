ae827b611c7d9b0e9868c33c001052289515953f
TalyaKasyanova <102550215+TalyaKasyanova@users.noreply.github.com>
2024-04-27 14:28:20 +0300

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("lamp").addEventListener("click", function () {
   const lamp = document.getElementById("lamp");
-  if (lamp.src.match("/images/lamp_off.png")) {
-    lamp.src = "/images/lamp_on.png";
+  if (lamp.src.match("./images/lamp_off.png")) {
+    lamp.src = "./images/lamp_on.png";
   } else {
-    lamp.src = "/images/lamp_off.png";
+    lamp.src = "./images/lamp_off.png";
   }
 });
