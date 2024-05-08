c81dc2514725eb05c37f4489c893d5e63219eb5a
TalyaKasyanova <102550215+TalyaKasyanova@users.noreply.github.com>
2024-05-09 01:39:33 +0300

    color lamp 2

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-document.getElementById("lamp").addEventListener("click", function () {
-  const lamp = document.getElementById("lamp");
-  const color = document.getElementById("colorPicker").value;
-  lamp.style.fill = color;
+const lamp = document.getElementById("lamp");
+const switchButton = document.getElementById("switch");
+const colorPicker = document.getElementById("colorPicker");
+
+switchButton.addEventListener("click", function () {
+  lamp.classList.toggle("lamp-on");
+  updateLampColor();
+  switchButtonText();
 });
+
+colorPicker.addEventListener("input", function () {
+  updateLampColor();
+});
+
+function updateLampColor() {
+  if (lamp.classList.contains("lamp-on")) {
+    lamp.style.filter = `drop-shadow(0 0 15px ${colorPicker.value})`;
+  } else {
+    lamp.style.filter = "none";
+  }
+}
+
+function switchButtonText() {
+  if (switchButton.textContent === "Turn On") {
+    switchButton.textContent = "Turn Off";
+  } else {
+    switchButton.textContent = "Turn On";
+  }
+}
